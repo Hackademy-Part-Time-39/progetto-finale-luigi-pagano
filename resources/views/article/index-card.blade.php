94cce738beb80db4f3da00d7a763d64049277662
luigipagano1990 <luigi.pagano25@gmail.com>
2024-10-30 17:07:36 +0100

    us 7 terminata e migliorato frontend

--- resources/views/article/index-card.blade.php
+++ resources/views/article/index-card.blade.php
@@ -1,19 +1,24 @@
 <x-layout>
     <div class="container mt-5">
-        <div class="row">
-            @foreach ($articles as $article)
-                <div class="col-md-4 mb-4">
-                    <x-article-card :article="$article" />
+        <div class="row justify-content-center">
+            @if ($articles->isEmpty())
+                <div class="col-12 text-center my-5">
+                    <p class="text-muted fs-4 fw-light">Non ci sono ricette pubblicate al momento.</p>
+                    
                 </div>
-            @endforeach
+            @else
+                @foreach ($articles as $article)
+                    <div class="col-md-4 mb-4 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="{{ $loop->index * 100 }}">
+                        <x-article-card :article="$article" />
+                    </div>
+                @endforeach
+            @endif
         </div>
 
         <div class="col-12 text-center mt-4">
-            <a href="{{ url('/') }}" class="btn btn-primary">Torna alla Home</a>
+            <a href="{{ url('/') }}" class="btn btn-primary btn-lg px-4 py-2 shadow-sm" role="button">Torna alla Home</a>
         </div>
     </div>
 
-    <script>
-        AOS.init();
-    </script>
+    
 </x-layout>
